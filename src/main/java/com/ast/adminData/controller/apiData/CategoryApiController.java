5c1476b630fd6dbef3289b11a959db7a76ef6b32
CGJ <qwertyu5258@naver.com>
2022-04-22 19:34:34 +0900

    CGJ Commit CategoryApiController

--- src/main/java/com/ast/adminData/controller/apiData/CategoryApiController.java
+++ src/main/java/com/ast/adminData/controller/apiData/CategoryApiController.java
@@ -43,33 +43,33 @@ public Map dp_cm_category_dt(@PathVariable String param1) {
 }
 //    dp_cm_category_save	분류 항목 저장	post	/dp/cm/category/save
 @PostMapping(value = {"/dp/cm/category/save"})
-public Map dp_cm_category_save() {
+public Map dp_cm_category_save(@RequestBody Map<String,Object> params) {
 
-    Map<String, Object> DataList = restService.postApi("/dp/cm/category/save");
+    Map<String, Object> DataList = restService.postApi("/dp/cm/category/save", params);
 
     return DataList;
 }
 //    dp_cm_category_update	분류 항목 수정	post	/dp/cm/category/update/{}
 @PostMapping(value = {"/dp/cm/category/update/{param1}"})
-public Map dp_cm_category_update(@PathVariable String param1) {
+public Map dp_cm_category_update(@PathVariable String param1, @RequestBody Map<String,Object> params) {
 
-    Map<String, Object> DataList = restService.postApi("/dp/cm/category/update/" + param1);
+    Map<String, Object> DataList = restService.postApi("/dp/cm/category/update/" + param1, params);
 
     return DataList;
 }
 //    dp_cm_category_del	분류 항목 삭제	post	/dp/cm/category/delete/{}
 @PostMapping(value = {"/dp/cm/category/delete/{param1}"})
-public Map dp_cm_category_del(@PathVariable String param1) {
+public Map dp_cm_category_del(@PathVariable String param1, @RequestBody Map<String,Object> params) {
 
-    Map<String, Object> DataList = restService.postApi("/dp/cm/category/delete/" + param1);
+    Map<String, Object> DataList = restService.postApi("/dp/cm/category/delete/" + param1, params);
 
     return DataList;
 }
 //    dp_cm_category_del_chk	분류 항목 삭제 전 하위분류 체크	post	/dp/cm/category/delete/check/{}
 @PostMapping(value = {"/dp/cm/category/delete/check/{param1}"})
-public Map dp_cm_category_del_chk(@PathVariable String param1) {
+public Map dp_cm_category_del_chk(@PathVariable String param1, @RequestBody Map<String,Object> params) {
 
-    Map<String, Object> DataList = restService.postApi("/dp/cm/category/delete/check/" + param1);
+    Map<String, Object> DataList = restService.postApi("/dp/cm/category/delete/check/" + param1, params);
 
     return DataList;
 }
